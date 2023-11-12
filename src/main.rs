85783e4870074b25f86c4af86482aa95ad7dd403
StephanA <s.avenwedde@gmail.com>
2023-11-12 21:38:24 +0100

    dev

---
+++ src/main.rs
@@ -0,0 +1,83 @@
+#![allow(unused_imports)]
+use crossterm::{
+    execute,
+    event::{self, KeyCode::Char, KeyEventKind, Event::Key},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::{
+    prelude::{CrosstermBackend, Stylize, Terminal},
+    widgets::Paragraph,
+};
+use std::io::{stdout};
+use anyhow::Result;
+pub type Frame<'a> = ratatui::Frame<'a>;
+
+struct App {
+    counter: i64,
+    should_quit: bool,
+}
+
+fn startup() -> Result<()> {
+    enable_raw_mode()?;
+    execute!(std::io::stderr(), EnterAlternateScreen)?;
+    Ok(())
+}
+
+fn shutdown() -> Result<()> {
+    execute!(std::io::stderr(), LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
+}
+
+fn ui(app: &App, f: &mut Frame<'_>) {
+    f.render_widget(Paragraph::new(format!("Counter: {}", app.counter)), f.size());
+}
+
+fn update(app: &mut App) -> Result<()> {
+    if event::poll(std::time::Duration::from_millis(250))? {
+      if let Key(key) = event::read()? {
+        if key.kind == event::KeyEventKind::Press {
+          match key.code {
+            Char('j') => app.counter += 1,
+            Char('k') => app.counter -= 1,
+            Char('q') => app.should_quit = true,
+            _ => {},
+          }
+        }
+      }
+    }
+    Ok(())
+}
+
+fn run() -> Result<()> {
+    // ratatui terminal
+    let mut t = Terminal::new(CrosstermBackend::new(std::io::stderr()))?;
+  
+    // application state
+    let mut app = App { counter: 0, should_quit: false };
+  
+    loop {
+      // application render
+      t.draw(|f| {
+        ui(&app, f);
+      })?;
+  
+      // application update
+      update(&mut app)?;
+  
+      // application exit
+      if app.should_quit {
+        break;
+      }
+    }
+  
+    Ok(())
+}
+fn main() -> Result<()> {
+    startup()?;
+    let status = run();
+    shutdown()?;
+    status?;
+    Ok(())
+}
