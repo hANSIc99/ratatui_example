e311082e4a56488a70b3aa89c2a61539cdfa7ffd
StephanA <s.avenwedde@gmail.com>
2023-11-19 22:18:29 +0100

    dev

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,146 @@
-/// Application.
-pub mod app;
+mod tui;
 
-/// Terminal events handler.
-pub mod event;
+use std::time::Duration;
 
-/// Widget renderer.
-pub mod ui;
+use color_eyre::eyre::Result;
+use crossterm::event::KeyCode::Char;
+use ratatui::{prelude::*, widgets::*};
+use tokio::sync::mpsc::{self, UnboundedSender};
+use tui::Event;
 
-/// Terminal user interface.
-pub mod tui;
+// App state
+struct App {
+  counter: i64,
+  should_quit: bool,
+  action_tx: UnboundedSender<Action>,
+}
 
-/// Application updater.
-pub mod update;
+// App actions
+#[derive(Clone)]
+pub enum Action {
+  Tick,
+  Increment,
+  Decrement,
+  NetworkRequestAndThenIncrement, // new
+  NetworkRequestAndThenDecrement, // new
+  Quit,
+  Render,
+  None,
+}
 
-use anyhow::Result;
-use app::App;
-use event::{Event, EventHandler};
-use ratatui::{backend::CrosstermBackend, Terminal};
-use tui::Tui;
-use update::update;
+// App ui render function
+fn ui(f: &mut Frame<'_>, app: &mut App) {
+  let area = f.size();
+  f.render_widget(
+    Paragraph::new(format!("Press j or k to increment or decrement.\n\nCounter: {}", app.counter,))
+      .block(
+        Block::default()
+          .title("ratatui async counter app")
+          .title_alignment(Alignment::Center)
+          .borders(Borders::ALL)
+          .border_type(BorderType::Rounded),
+      )
+      .style(Style::default().fg(Color::Cyan))
+      .alignment(Alignment::Center),
+    area,
+  );
+}
 
-fn main() -> Result<()> {
-  // Create an application.
-  let mut app = App::new();
+fn get_action(_app: &App, event: Event) -> Action {
+  match event {
+    Event::Error => Action::None,
+    Event::Tick => Action::Tick,
+    Event::Render => Action::Render,
+    Event::Key(key) => {
+      match key.code {
+        Char('j') => Action::Increment,
+        Char('k') => Action::Decrement,
+        Char('J') => Action::NetworkRequestAndThenIncrement, // new
+        Char('K') => Action::NetworkRequestAndThenIncrement, // new
+        Char('q') => Action::Quit,
+        _ => Action::None,
+      }
+    },
+    _ => Action::None,
+  }
+}
 
-  // Initialize the terminal user interface.
-  let backend = CrosstermBackend::new(std::io::stderr());
-  let terminal = Terminal::new(backend)?;
-  let events = EventHandler::new(250);
-  let mut tui = Tui::new(terminal, events);
+fn update(app: &mut App, action: Action) {
+  match action {
+    Action::Increment => {
+      app.counter += 1;
+    },
+    Action::Decrement => {
+      app.counter -= 1;
+    },
+    Action::NetworkRequestAndThenIncrement => {
+      let tx = app.action_tx.clone();
+      tokio::spawn(async move {
+        tokio::time::sleep(Duration::from_secs(5)).await; // simulate network request
+        tx.send(Action::Increment).unwrap();
+      });
+    },
+    Action::NetworkRequestAndThenDecrement => {
+      let tx = app.action_tx.clone();
+      tokio::spawn(async move {
+        tokio::time::sleep(Duration::from_secs(5)).await; // simulate network request
+        tx.send(Action::Decrement).unwrap();
+      });
+    },
+    Action::Quit => app.should_quit = true,
+    _ => {},
+  };
+}
+
+async fn run() -> Result<()> {
+  let (action_tx, mut action_rx) = mpsc::unbounded_channel(); // new
+
+  // ratatui terminal
+  let mut tui = tui::Tui::new()?.tick_rate(1.0).frame_rate(30.0);
   tui.enter()?;
 
-  //Start the main loop.
-  while !app.should_quit {
-    // Render the user interface.
-    tui.draw(&mut app)?;
-    // Handle events.
-    match tui.events.next()? {
-      Event::Tick => {},
-      Event::Key(key_event) => update(&mut app, key_event),
-      Event::Mouse(_) => {},
-      Event::Resize(_, _) => {},
-    };
-  }
+  // application state
+  let mut app = App { counter: 0, should_quit: false, action_tx: action_tx.clone() };
 
-  // Exit the user interface.
+  loop {
+    let e = tui.next().await?;
+    match e {
+      tui::Event::Quit => action_tx.send(Action::Quit)?,
+      tui::Event::Tick => action_tx.send(Action::Tick)?,
+      tui::Event::Render => action_tx.send(Action::Render)?,
+      tui::Event::Key(_) => {
+        let action = get_action(&app, e);
+        action_tx.send(action.clone())?;
+      },
+      _ => {},
+    };
+
+    while let Ok(action) = action_rx.try_recv() {
+      // application update
+      update(&mut app, action.clone());
+      // render only when we receive Action::Render
+      if let Action::Render = action {
+        tui.draw(|f| {
+          ui(f, &mut app);
+        })?;
+      }
+    }
+
+    // application exit
+    if app.should_quit {
+      break;
+    }
+  }
   tui.exit()?;
+
+  Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+  let result = run().await;
+
+  result?;
+
   Ok(())
 }
--- src/tui.rs
+++ src/tui.rs
@@ -1,28 +1,141 @@
-use std::{io, panic};
-
+use crossterm::event::{KeyEvent, MouseEvent, EventStream};
+use crossterm::event::{Event as CrosstermEvent};
+use futures::{StreamExt, FutureExt};
+use tokio::{sync::mpsc::{self, UnboundedReceiver, UnboundedSender}, task::JoinHandle};
 use anyhow::Result;
-use crossterm::{
-  event::{DisableMouseCapture, EnableMouseCapture},
-  terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+use ratatui::{
+  prelude::{CrosstermBackend, Terminal},
+  widgets::Paragraph,
 };
 
-pub type CrosstermTerminal = ratatui::Terminal<ratatui::backend::CrosstermBackend<std::io::Stderr>>;
+#[derive(Clone, Debug)]
+pub enum Event {
+  Init,
+  Quit,
+  Error,
+  Closed,
+  Tick,
+  Render,
+  FocusGained,
+  FocusLost,
+  Paste(String),
+  Key(KeyEvent),
+  Mouse(MouseEvent),
+  Resize(u16, u16),
+}
 
-use crate::{app::App, event::EventHandler, ui};
+// #[derive(Clone, Copy, Debug)]
+// pub enum Event {
+//   Error,
+//   Tick,
+//   Key(KeyEvent),
+// }
 
-/// Representation of a terminal user interface.
-///
-/// It is responsible for setting up the terminal,
-/// initializing the interface and handling the draw events.
+#[derive(Debug)]
+pub struct EventHandler {
+  _tx: UnboundedSender<Event>,
+  rx: UnboundedReceiver<Event>,
+  task: Option<JoinHandle<()>>,
+}
+
+impl EventHandler {
+  pub fn new() -> Self {
+    let tick_rate = std::time::Duration::from_millis(250);
+
+    let (tx, rx) = mpsc::unbounded_channel();
+    let _tx = tx.clone();
+
+    let task = tokio::spawn(async move {
+      let mut reader = crossterm::event::EventStream::new();
+      let mut interval = tokio::time::interval(tick_rate);
+      loop {
+        let delay = interval.tick();
+        let crossterm_event = reader.next().fuse();
+        tokio::select! {
+          maybe_event = crossterm_event => {
+            match maybe_event {
+              Some(Ok(evt)) => {
+                match evt {
+                  crossterm::event::Event::Key(key) => {
+                    if key.kind == crossterm::event::KeyEventKind::Press {
+                      tx.send(Event::Key(key)).unwrap();
+                    }
+                  },
+                  _ => {},
+                }
+              }
+              Some(Err(_)) => {
+                tx.send(Event::Error).unwrap();
+              }
+              None => {},
+            }
+          },
+          _ = delay => {
+              tx.send(Event::Tick).unwrap();
+          },
+        }
+      }
+    });
+
+    Self { _tx, rx, task: Some(task) }
+  }
+
+  pub async fn next(&mut self) -> Result<Event> {
+    self.rx.recv().await.ok_or(color_eyre::eyre::eyre!("Unable to get event"))
+  }
+}
+
 pub struct Tui {
-    /// Interface to the Terminal.
-    terminal: CrosstermTerminal,
-    /// Terminal event handler.
-    pub events: EventHandler,
+  pub terminal: ratatui::Terminal<CrosstermBackend<std::io::Stderr>>,
+  pub task: JoinHandle<()>,
+  pub event_rx: UnboundedReceiver<Event>,
+  pub event_tx: UnboundedSender<Event>,
+  pub frame_rate: f64,
+  pub tick_rate: f64,
 }
 
 impl Tui {
-    /// Constructs a new instance of [`Tui`].
+  pub fn start(&mut self) {
+    let tick_delay = std::time::Duration::from_secs_f64(1.0 / self.tick_rate);
+    let render_delay = std::time::Duration::from_secs_f64(1.0 / self.frame_rate);
+    let _event_tx = self.event_tx.clone();
+    self.task = tokio::spawn(async move {
+      let mut reader = crossterm::event::EventStream::new();
+      let mut tick_interval = tokio::time::interval(tick_delay);
+      let mut render_interval = tokio::time::interval(render_delay);
+      loop {
+        let tick_delay = tick_interval.tick();
+        let render_delay = render_interval.tick();
+        let crossterm_event = reader.next().fuse();
+        tokio::select! {
+          maybe_event = crossterm_event => {
+            match maybe_event {
+              Some(Ok(evt)) => {
+                match evt {
+                  CrosstermEvent::Key(key) => {
+                    if key.kind == KeyEventKind::Press {
+                      _event_tx.send(Event::Key(key)).unwrap();
+                    }
+                  },
+                }
+              }
+              Some(Err(_)) => {
+                _event_tx.send(Event::Error).unwrap();
+              }
+              None => {},
+            }
+          },
+          _ = tick_delay => {
+              _event_tx.send(Event::Tick).unwrap();
+          },
+          _ = render_delay => {
+              _event_tx.send(Event::Render).unwrap();
+          },
+        }
+      }
+    });
+  }
+      /// Constructs a new instance of [`Tui`].
     pub fn new(terminal: CrosstermTerminal, events: EventHandler) -> Self {
       Self { terminal, events }
     }
